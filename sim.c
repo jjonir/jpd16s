55b30765be7da109ae4bcb4524ebe77aaa837ecb
jpendergrast <jpendergrast@insulet.com>
2012-11-13 11:31:30 -0500

    Modified disassembly dump to better handle loops and small jumps.

--- sim.c
+++ sim.c
@@ -6,6 +6,7 @@
 #include "../jpd16a/disasm.h" // TODO don't do this
 
 static void dump_state(void);
+static void dump_disassembly(void);
 static int load_bin(const char *filename);
 
 int main(int argc, char *argv[])
@@ -16,7 +17,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++) {
 		if ((strcmp(argv[i], "-d") == 0) ||
-		    (strcmp(argv[i], "--debug") == 0))
+			(strcmp(argv[i], "--debug") == 0))
 			dbg = 1;
 		else
 			strncpy(corefilename, argv[i], 1023);
@@ -44,8 +45,6 @@ void dump_state(void)
 {
 	int i, inst_len;
 	uint16_t addr;
-	static uint16_t past_addr[5] = {0, 0, 0, 0, 0};
-	int prev_insts;
 	char buf[32];
 
 	mvprintw(0,  0, "Clock: 0x%.4x", clock);
@@ -65,33 +64,7 @@ void dump_state(void)
 	mvprintw(13, 0, "| IA: 0x%.4x |", registers.IA);
 	mvprintw(14, 0, "+------------+");
 
-	mvprintw(1, 16, "+---------------------------+");
-	prev_insts = 0;
-	for (i = 0; i < 4; i++)
-		past_addr[i] = past_addr[i + 1];
-	past_addr[4] = registers.PC;
-	for (i = 3; i >= 0; i--) {
-		addr = past_addr[i];
-		inst_len = decode_inst(&memory[addr], buf);
-		if ((inst_len + addr) == past_addr[i + 1])
-			prev_insts++;
-	}
-	for (i = 0; i < prev_insts; i++) {
-		addr = past_addr[i + (4 - prev_insts)];
-		inst_len = decode_inst(&memory[addr], buf);
-		mvprintw(i + 2, 16, "|                           |");
-		mvprintw(i + 2, 16, "| %.4x %s", addr, buf);
-	}
-	for (addr = registers.PC; i < 12; i++, addr += inst_len) {
-		inst_len = decode_inst(&memory[addr], buf);
-		mvprintw(i + 2, 16, "|                           |");
-		if (addr == registers.PC)
-			attron(A_BOLD);
-		mvprintw(i + 2, 18,   "%.4x %s", addr, buf);
-		if (addr == registers.PC)
-			attroff(A_BOLD);
-	}
-	mvprintw(14,16, "+---------------------------+");
+	dump_disassembly();
 
 	for (i = 0; i < 0x10; i++)
 		mvprintw(16 + i, 0,
@@ -110,6 +83,44 @@ void dump_state(void)
 	attroff(A_BOLD);
 
 	refresh();
+}
+
+uint16_t addr_buf[12];
+void dump_disassembly(void)
+{
+	int i, pc_index, inst_len, offset;
+	uint16_t addr;
+	char buf[32];
+
+	pc_index = 0;
+	for (i = 0; i < 12; i++) {
+		if (addr_buf[i] == registers.PC) {
+			pc_index = i;
+			break;
+		}
+	}
+	offset = 0;
+	if (pc_index > 5)
+		offset = pc_index - 5;
+
+	mvprintw(1, 16, "+---------------------------+");
+	for (i = 0; (i + offset) < pc_index; i++) {
+		decode_inst(&memory[addr_buf[i + offset]], buf);
+		mvprintw(i + 2, 16, "|                           |");
+		mvprintw(i + 2, 16, "| %.4x %s", addr_buf[i + offset], buf);
+		addr_buf[i] = addr_buf[i + offset];
+	}
+	for (addr = registers.PC; i < 12; i++, addr += inst_len) {
+		inst_len = decode_inst(&memory[addr], buf);
+		mvprintw(i + 2, 16, "|                           |");
+		if (addr == registers.PC)
+			attron(A_BOLD);
+		mvprintw(i + 2, 18,   "%.4x %s", addr, buf);
+		if (addr == registers.PC)
+			attroff(A_BOLD);
+		addr_buf[i] = addr;
+	}
+	mvprintw(14,16, "+---------------------------+");
 }
 
 int load_bin(const char *filename)
